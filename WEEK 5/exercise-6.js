c5776b8c35a893b4d51c50ccc4bcac80919e9174
muhammadkhevin <40518949+vinutama@users.noreply.github.com>
2018-07-27 17:00:48 +0700

    adding exercise 5-8

---
+++ WEEK 5/exercise-6.js
@@ -0,0 +1,45 @@
+
+// Diberikan function meleeRangedGrouping yang menerima 1 parameter berupa string,
+// implementasikan meleeRangedGrouping agar dapat menghasilkan multidimensional array seperti yang diminta.
+
+// Format string yang diberikan adalah: <nama_hero>-<tipe_hero>,<nama_hero>-<tipe-hero>, ...
+
+// Output yang diharapkan: [ [ <daftar_hero_dengan_tipe_ranged> ], [ <daftar_hero_dengan_tipe_melee> ] ]
+
+// Jika input adalah string kosong ('') maka return array kosong
+
+function meleeRangedGrouping(str) {
+    if (str === '') {
+        return [];
+    } else {
+
+
+        var splitStr = str.split('-').join(',');
+        var lastSplit = splitStr.split(',');
+        //console.log(splitStr)
+        //console.log(lastSplit)
+        var newArr = [];
+        var newArr2 = [];
+        var finalArr = [];
+        for (var i = 0; i < lastSplit.length; i++) {
+            if (lastSplit[i] === 'Ranged') {
+                newArr.push(lastSplit[i - 1]);
+            } else if (lastSplit[i] === 'Melee') {
+                newArr2.push(lastSplit[i - 1]);
+            }
+        }
+        finalArr.push(newArr);
+        finalArr.push(newArr2);
+        return finalArr;
+    }
+}
+
+// TEST CASE
+
+console.log(meleeRangedGrouping('Razor-Ranged,Invoker-Ranged,Meepo-Melee,Axe-Melee,Sniper-Ranged'));
+// [ ['Razor', 'Invoker', 'Sniper'], ['Meepo', 'Axe'] ]
+
+console.log(meleeRangedGrouping('Drow Ranger-Ranged,Chen-Ranged,Dazzle-Ranged,Io-Ranged'));
+// [ ['Drow Ranger', 'Chen', 'Dazzle', 'Io'], [] ]
+
+console.log(meleeRangedGrouping('')); // []
---
+++ WEEK 5/exercise-8.js
@@ -0,0 +1,10 @@
+function cariPelaku(str) {
+    return str.match(/abc/gi, '').length;
+}
+
+// TEST CASES
+console.log(cariPelaku('mabcvabc')); // 2
+console.log(cariPelaku('abcdabdc')); // 1
+console.log(cariPelaku('bcabcac')); // 1
+console.log(cariPelaku('abcabcabc')); // 3
+console.log(cariPelaku('babcbacabc')); // 2
